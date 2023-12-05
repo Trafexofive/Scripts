1d9546a652c0f91ce29f41248ba73594c45eaa99
Trafexofive <les5phenix@gmail.com>
2023-12-06 00:35:00 +0100

    brew-script added mac-cleanup

--- brew_goinfree.sh
+++ brew_goinfree.sh
@@ -2,11 +2,13 @@ brewcachedel
 
 if ! command -v brew &> /dev/null
 then
-  export HOME_BREW="/Users/$USER/goinfre/$USER"
   rm -rf $HOME/.brew && rm -rf $HOME_BREW/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME_BREW/.brew
 
 fi
 
-curl -O "https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.7.0/Tachidesk-Server-v0.7.0-r1197-macOS-x64.zip"
+#curl -O "https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.7.0/Tachidesk-Server-v0.7.0-r1197-macOS-x64.zip"
 
-brew install lua lsd
+
+brew install lua tree fzf 
+brew tap fwartner/tap
+brew install fwartner/tap/mac-cleanup
