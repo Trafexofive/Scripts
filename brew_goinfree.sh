77d140c580b7a82543eab002a10a7cf8d67aea04
Trafexofive <les5phenix@gmail.com>
2024-02-27 21:04:10 +0100

    small changes to brew script

--- brew_goinfree.sh
+++ brew_goinfree.sh
@@ -1,5 +1,6 @@
 export HOME_BREW="/Users/mlamkadm/goinfre/mlamkadm"
 export HOMEBREW_CACHE="/Users/mlamkadm/goinfre/mlamkadm/.cache"
+
 if ! command -v brew &>/dev/null; then
 	rm -rf $HOME/.brew && rm -rf $HOME_BREW/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME_BREW/.brew
 	mkdir /Users/mlamkadm/goinfre/mlamkadm/.cache
@@ -8,7 +9,7 @@ fi
 
 #curl -O "https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.7.0/Tachidesk-Server-v0.7.0-r1197-macOS-x64.zip"
 
-packages=("go" "lua" "gh" "node" "lazygit" "tree" "fzf" "neofetch")
+packages=("go" "lua" "gh" "node" "bat" "lazygit" "tree" "fzf" "neofetch")
 
 # Iterate over the array and install each package
 for package in "${packages[@]}"; do
