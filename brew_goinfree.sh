ed4d1c122ff5408765302f642d65fbc314d793cb
Trafexofive <les5phenix@gmail.com>
2024-03-28 03:28:48 +0000

    m

--- brew_goinfree.sh
+++ brew_goinfree.sh
@@ -9,7 +9,7 @@ fi
 
 #curl -O "https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.7.0/Tachidesk-Server-v0.7.0-r1197-macOS-x64.zip"
 
-packages=("go" "lua" "gh" "node" "bat" "lazygit" "tree" "fzf" "neofetch")
+packages=("go" "lua" "gh" "node" "bat" "ripgrep" "lazygit" "tree" "fzf" "neofetch")
 
 # Iterate over the array and install each package
 for package in "${packages[@]}"; do
